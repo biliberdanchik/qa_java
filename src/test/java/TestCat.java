a0d40750a089000519df18b6486912bdb6586075
Max Alekseev <m.alekseev10@yandex.ru>
2025-01-20 15:24:00 +0300

    added tests for Cat

---
+++ src/test/java/TestCat.java
@@ -0,0 +1,30 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestCat {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void checkGetSound() {
+        Cat cat = new Cat(feline);
+        String actualSound = cat.getSound();
+        assertEquals("Мяу", actualSound);
+    }
+
+    @Test
+    public void checkGetFood() throws Exception {
+        Cat cat = new Cat(feline);
+        cat.getFood();
+        Mockito.verify(feline, Mockito.times(1)).eatMeat();
+    }
+}
