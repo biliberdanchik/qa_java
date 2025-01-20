22303daf4e9004b54d1f5d3814daf0a9204e8185
Max Alekseev <m.alekseev10@yandex.ru>
2025-01-20 21:49:37 +0300

    added tests for Lion

---
+++ src/test/java/lion/tests/TestLion.java
@@ -0,0 +1,41 @@
+package lion.tests;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestLion {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void checkGetKittens() throws Exception {
+        Lion lion = new Lion(feline, "Самка");
+        lion.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens();
+    }
+
+    @Test
+    public void checkGetFood() throws Exception {
+        Lion lion = new Lion(feline, "Самец");
+        lion.getFood();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+
+    @Test
+    public void checkThrowsExceptionLionConstructor() throws Exception {
+        Lion lion = new Lion(feline, "Небинарная личность");
+        InterruptedException exception = assertThrows(InterruptedException.class, () -> lion.getFood());
+        assertEquals("Используйте допустимые значения пола животного - самец или самка", exception.getMessage());
+    }
+
+}
---
+++ src/test/java/lion/tests/TestLionParam.java
@@ -0,0 +1,39 @@
+package lion.tests;
+
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class TestLionParam {
+    private final String sex;
+    private final boolean expected;
+
+    public TestLionParam(String sex, boolean expected) {
+        this.sex = sex;
+        this.expected = expected;
+    }
+
+    @Parameters
+    public static Object[][] getLionData() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void checkLionConstructor() throws Exception {
+        Feline feline = Mockito.mock(Feline.class);
+        Lion lion = new Lion(feline, sex);
+        assertEquals(expected, lion.doesHaveMane());
+
+    }
+
+}
